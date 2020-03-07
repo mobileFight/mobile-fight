a2641922acc31ed37daaf8782f553f870571efbb
Dmitry Shelomanov <dmitryshelomanov@mail.ru>
2020-03-08 01:40:42 +0300

    Updates

---
+++ precache-manifest.0d9ed583d10a0e00959ec853a8fff52d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2332673e174299c4d48596d164d09185",
+    "url": "/mobileFight/mobile-fight/index.html"
+  },
+  {
+    "revision": "50018846254a4b705e98",
+    "url": "/mobileFight/mobile-fight/static/js/2.808be3cb.chunk.js"
+  },
+  {
+    "revision": "b55e4b261299b825d57f",
+    "url": "/mobileFight/mobile-fight/static/js/main.50066655.chunk.js"
+  },
+  {
+    "revision": "d88f087fe626bff0f65c",
+    "url": "/mobileFight/mobile-fight/static/js/runtime~main.e5ca6de2.js"
+  },
+  {
+    "revision": "71e3e185e2425fbda028d18abff8b6fc",
+    "url": "/mobileFight/mobile-fight/static/media/frame.71e3e185.png"
+  },
+  {
+    "revision": "db910f315c14999717ced7cd70c0e078",
+    "url": "/mobileFight/mobile-fight/static/media/location.db910f31.jpg"
+  }
+]);
